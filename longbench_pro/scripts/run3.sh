6555c4c01a59392fcbbff65cd45181e5051d567b
“wyang” <wyang107@ucr.edu>
2024-10-07 14:32:27 -0400

    1

---
+++ longbench_pro/scripts/run3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash  
+#SBATCH --job-name=JOBNAME 
+#SBATCH --partition=gpu           
+#SBATCH --nodes=1                 
+#SBATCH -n 64    
+#SBATCH --gres=gpu:h100:1
+#SBATCH -A PDS0352
+#SBATCH --output=/users/PDS0352/wyang107/project/LCEG/longbench_pro/result/output.txt      
+#SBATCH --error=/users/PDS0352/wyang107/project/LCEG/longbench_pro/result/error.txt   
+#SBATCH --time=0-10:00:00
+
+conda activate lceg2
+python /users/PDS0352/wyang107/project/LCEG/longbench_pro/pred.py
+
