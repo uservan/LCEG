274b2fa24df2992bd4281b90c1028ec550c7020e
“wyang” <wyang107@ucr.edu>
2024-10-13 17:55:57 -0400

    1

--- longbench_pro/scripts/run2.sh
+++ longbench_pro/scripts/run2.sh
@@ -5,8 +5,8 @@
 #SBATCH -n 64    
 #SBATCH --gres=gpu:h100:1
 #SBATCH -A PDS0352
-#SBATCH --output=/users/PDS0352/wyang107/project/LCEG/longbench_pro/result/output5.txt      
-#SBATCH --error=/users/PDS0352/wyang107/project/LCEG/longbench_pro/result/error5.txt   
+#SBATCH --output=/users/PDS0352/wyang107/project/LCEG/longbench_pro/result/output.txt      
+#SBATCH --error=/users/PDS0352/wyang107/project/LCEG/longbench_pro/result/error.txt   
 #SBATCH --time=0-100:00:00
 
-python /users/PDS0352/wyang107/project/LCEG/longbench_pro/pred.py --model Qwen2.5-14B-Instruct
+python /users/PDS0352/wyang107/project/LCEG/longbench_pro/pred.py --model llama-3.1-8B-Instruct
