0c05589e018d4455ee7cbca14040ed7888d972b0
“wyang” <wyang107@ucr.edu>
2024-10-14 15:00:28 -0400

    1

--- longbench/scripts/run.sh
+++ longbench/scripts/run.sh
@@ -5,8 +5,8 @@
 #SBATCH -n 64    
 #SBATCH --gres=gpu:a100:1
 #SBATCH -A PDS0352
-#SBATCH --output=/users/PDS0352/wyang107/project/LCEG/longbench/result/output.txt      
-#SBATCH --error=/users/PDS0352/wyang107/project/LCEG/longbench/result/error.txt   
+#SBATCH --output=/users/PDS0352/wyang107/project/LCEG/longbench/result/output2.txt      
+#SBATCH --error=/users/PDS0352/wyang107/project/LCEG/longbench/result/error2.txt   
 #SBATCH --time=0-10:00:00
 
-python /users/PDS0352/wyang107/project/LCEG/longbench/pred.py --model llama2-7b-hf-slimpajama-landmark
+python /users/PDS0352/wyang107/project/LCEG/longbench/pred.py --model llama2-7b-hf
